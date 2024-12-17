1d168b7981929c2552e2ca110b02f4442b901355
aliahasan <aanabin18@gmail.com>
2024-12-17 12:41:18 +0600

    updated

--- src/app/modules/Course/course.route.ts
+++ src/app/modules/Course/course.route.ts
@@ -1,31 +1,40 @@
 import express from 'express';
+import auth from '../../middleware/auth';
 import validateRequest from '../../middleware/validateRequest';
+import { USER_ROLE } from '../user/user.constant';
 import { CourseControllers } from './course.controller';
 import { CourseValidations } from './course.validation';
 
 const router = express.Router();
 
 router.post(
-  '/',
+  '/create-course',
+  auth(USER_ROLE.admin),
   validateRequest(CourseValidations.createCourseValidationSchema),
   CourseControllers.createCourse,
 );
 
 router.get('/', CourseControllers.getAllCourses);
 
-router.get('/:id', CourseControllers.getSingleCourse);
+router.get(
+  '/:id',
+  auth('student', 'faculty', 'admin'),
+  CourseControllers.getSingleCourse,
+);
 
 router.patch(
   '/:id',
+  auth('admin'),
   validateRequest(CourseValidations.updateCourseValidationSchema),
   CourseControllers.updateCourse,
 );
 
-router.delete('/:id', CourseControllers.deleteCourse);
+router.delete('/:id', auth('admin'), CourseControllers.deleteCourse);
 
 // assign faculties to the courses
 router.put(
   '/:courseId/assign-faculties',
+  auth('admin'),
   validateRequest(CourseValidations.facultiesWithCourseValidationSchema),
   CourseControllers.assignFacultiesWithCourse,
 );
@@ -33,6 +42,7 @@ router.put(
 // remove faculties from Courses
 router.delete(
   '/:courseId/remove-faculties',
+  auth('admin'),
   validateRequest(CourseValidations.facultiesWithCourseValidationSchema),
   CourseControllers.removeFacultiesFromCourse,
 );
