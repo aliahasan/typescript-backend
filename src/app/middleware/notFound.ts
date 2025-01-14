23120c5e84f7b310ea1d26fc8fc114023184999c
aliahasan <aanabin18@gmail.com>
2025-01-15 00:24:24 +0600

    updated

--- src/app/middleware/notFound.ts
+++ src/app/middleware/notFound.ts
@@ -1,16 +1,17 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable no-unused-vars */
 import { NextFunction, Request, Response } from 'express';
-import httpStatus from 'http-status-codes';
+import { StatusCodes } from 'http-status-codes';
 
 const handleNotFoundRoute = async (
   req: Request,
   res: Response,
   next: NextFunction,
 ) => {
-  res.status(httpStatus.NOT_FOUND).json({
+  return res.status(StatusCodes.NOT_FOUND).json({
     success: false,
-    message: `Api not found!`,
+    message: 'API Not Found !!',
+    error: `Route ${req.originalUrl} not found}`,
   });
 };
 
